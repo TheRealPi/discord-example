fbb9b2a7a80614d97b7d576e2618a3a82f9da6f8
TheRealToast <33233103+TheRealPi@users.noreply.github.com>
2019-05-09 18:46:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,14 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === '$ping') {
     	message.reply('pong');
   	}
 });
-
+client.on('message', message => {
+    if (message.content === '$help') {
+    	message.reply('$ping: Pong!');
+  	}
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
